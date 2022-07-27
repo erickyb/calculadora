c3313cb1bcb4cd8cce144de0c29a2f5533deea85
Erick Yarleque B <102404443+erickyb@users.noreply.github.com>
2022-07-27 02:20:48 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// suma resta multiplicacion division
+function suma() {
+  resultado = 5 + 6
+  console.log(resultado)
+ }
+suma()
+function resta () {
+  resultado_Resta = 10 - 5
+  console.log(resultado_Resta)
+}
+resta()
+function multiplicacion () {
+  resultado_Multi =5*2
+  console.log (resultado_Multi)
+}
+multiplicacion ()
+
+function division () {
+  resultado_Div = 15/5
+  console.log (resultado_Div)
+}
+division ()
